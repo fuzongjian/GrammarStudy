d720b4aafeb4ec56598f833b257eeeb9edfc6e88
fuzongjian <2917066078@qq.com>
2016-05-21 16:07:50 +0800

    继续收集问题

--- GrammarStudy/内存问题.h
+++ GrammarStudy/内存问题.h
@@ -24,9 +24,13 @@
      MRC工程中也可以使用ARC的类，方法如下：
      在XCode中targets的build phases选项下Compile Sources下选择要使用arc编译的文件
      双击输入 -fobjc-arc 即可
- 
- 
- 
+ 四、内存管理基本原则
+    一、MRC
+    OC内存管理遵循“谁创建，谁释放，谁引用，谁管理”的机制，当创建或者引用一个对象的时候，需要向它发送alloc、copy、retain消息，当释放该对象发送release消息，当对象引用计数为0是，系统将释放该对象，这是OC的手动管理机制（MRC）
+    二、ARC
+    a、iOS5.0之后引用自动管理机制——自动引用计数（ARC），管理机制和手动管理机制类似，只是不再需要调用retain、release、autorelease；
+    b、它编译的特性，当你使用ARC时，在适当的位置插入release和autorelese；
+    c、strong修饰的指针变量指向对象时，当指针指向新值或者指针不复存在，相关联对象就会被自动释放，而weak修饰的指针变量指向对象，当对象的拥有者指向新值或者不存在时weak修饰的指针会自动置为nil
  
  
  
