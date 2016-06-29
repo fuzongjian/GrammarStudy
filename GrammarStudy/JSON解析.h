ef9f8d480c5b72281fe31a0f8314c79ffc22d535
fuzongjian <fu@163.com>
2016-06-29 14:09:44 +0800

    JSON解析参数

---
+++ GrammarStudy/JSON解析.h
@@ -0,0 +1,35 @@
+//
+//  JSON解析.h
+//  GrammarStudy
+//
+//  Created by Shao Jie on 16/6/29.
+//  Copyright © 2016年 speeda. All rights reserved.
+//
+
+#ifndef JSON___h
+#define JSON___h
+
+/*
+ 在解析JSON方法NSJSONReadingOptions参数:
+ NSJSONReadingOptions
+ 
+ NSJSONReadingMutableContainers = (1UL << 0)
+ 
+ 创建出来的数组和字典就是可变
+ 
+ NSJSONReadingMutableLeaves = (1UL << 1)
+ 
+ 数组或者字典里面的字符串是可变的
+ 
+ NSJSONReadingAllowFragments
+ 
+ 允许解析出来的对象不是字典或者数组，比如直接是字符串或者NSNumber
+ 
+ KNilOptions
+ 
+ 如果不在乎服务器返回的是可变的还是不可变的，直接传入KNilOptions，效率最高！返回的就是不可变的
+ 
+
+ 
+ */
+#endif /* JSON___h */
